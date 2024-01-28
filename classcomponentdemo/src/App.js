871b65adef64856fc990e322391be6b24fde7b24
AnkitLipate <130392155+AnkitLipate@users.noreply.github.com>
2024-01-28 12:26:10 +0530

    Add files via upload

---
+++ classcomponentdemo/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello World !!</h1>
+        <h2>Welcome to react App from class component</h2>
+        <h3>This is Ankit completed PG-DAC from CDAC, Pune</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
